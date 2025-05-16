ce559413027acf7f18e67e6fc6194f11724c5441
Nikita512 <singh502nikita@gmail.com>
2025-05-16 21:28:13 +0530

    Validation

---
+++ Backend/middleware/validation.js
@@ -0,0 +1,33 @@
+const { body, validationResult } = require('express-validator');
+
+const validateRegistration = [
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long'),
+    body('name').notEmpty().withMessage('Name is required'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
+
+const validateLogin = [
+    body('email').isEmail().withMessage('Please enter a valid email'),
+    body('password').notEmpty().withMessage('Password is required'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+];
+
+module.exports = {
+    validateRegistration,
+    validateLogin
+}; 
